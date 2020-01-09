c40c0c914192c169b60de781e3ec047e2b8a7cc3
Peter Moser <p.moser@noi.bz.it>
2020-01-09 08:59:45 +0100

    Build: Update Jenkinsfile-Test.groovy

--- Jenkinsfile-Test.groovy
+++ Jenkinsfile-Test.groovy
@@ -65,7 +65,7 @@ pipeline {
         }
         stage('Deploy') {
             steps{
-                sh 'mvn -B -U -Dmaven.test.skip=true tomcat:redeploy -Dmaven.tomcat.url=${TESTSERVER_TOMCAT_ENDPOINT} -Dmaven.tomcat.server=testServer -Dmaven.tomcat.path=/'
+                sh 'mvn -B -U -Pproduction-war -Dmaven.test.skip=true tomcat:redeploy -Dmaven.tomcat.url=${TESTSERVER_TOMCAT_ENDPOINT} -Dmaven.tomcat.server=testServer -Dmaven.tomcat.path=/'
             }
         }
     }
