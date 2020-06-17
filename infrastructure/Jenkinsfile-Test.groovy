1ebda572f3ba4294503fcff0918c135ab38fefdd
Alex Lanz <alex@krumer.it>
2020-06-17 16:15:55 +0200

    Fixing deployment

--- infrastructure/Jenkinsfile-Test.groovy
+++ infrastructure/Jenkinsfile-Test.groovy
@@ -55,7 +55,7 @@ pipeline {
             steps {
                 sh '''
                     docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
-                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app mvn -B -U clean test
+                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app "mvn -B -U clean test"
                 '''
             }
         }
--- infrastructure/docker/entrypoint.sh
+++ infrastructure/docker/entrypoint.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
 
-mkdir -p ~/.m2
-
-cat > ~/.m2/settings.xml << EOF
-<settings>
-    <localRepository>$PWD/infrastructure/docker/.m2</localRepository>
-</settings>
-EOF
-
 cp /code/src/main/resources/application.properties.example /code/src/main/resources/application.properties
 cp /code/src/main/resources/application.users-file.example /code/src/main/resources/application.users-file
 
