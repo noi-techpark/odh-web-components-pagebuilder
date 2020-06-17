a710ceb41640a5062e2051dc3fc31a718a901c9c
Alex Lanz <alex@krumer.it>
2020-06-17 16:12:10 +0200

    Fixing deployment

--- infrastructure/Jenkinsfile-CI.groovy
+++ infrastructure/Jenkinsfile-CI.groovy
@@ -1,15 +1,18 @@
 pipeline {
-    agent {
-        dockerfile {
-            filename 'infrastructure/docker/Dockerfile'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
-        }
-    }
+    agent any
 
     stages {
+        stage('Configure') {
+            steps {
+                sh 'cp .env.example .env'
+            }
+        }
         stage('Test') {
             steps {
-                sh 'mvn -B -U clean test'
+                sh '''
+                    docker-compose --no-ansi build --pull --build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)
+                    docker-compose --no-ansi run --rm --no-deps -u $(id -u jenkins):$(id -g jenkins) app mvn -B -U clean test
+                '''
             }
         }
     }
